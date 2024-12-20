c637f0afec776fa11cf53f13444b09fc7f067660
h4sh5 <h4sh5@protonmail.com>
2024-12-21 08:34:57 +1000

    range distribution

--- forkfetch.py
+++ forkfetch.py
@@ -11,10 +11,10 @@ def eprint(s, *args, **kwargs):
 def usage():
 	print('''Fetchfork distributes HTTP chunks across multiple SSH remotes to accelerate download
 	-h	help
-	-r	SSH remotes (comma separated, spaces ignored, passed to ssh command line. Please have publickey or sshagent setup since fetchfork doesn't prompt for passwords). Either curl or wget must be installed on remote servers, and only linux is supported
+	-r	SSH remotes (comma separated, spaces ignored, passed to ssh command line. Please have publickey or sshagent setup since fetchfork doesn't prompt for passwords). curl must be installed on remote servers (wget support might come later)
 	-H	HTTP header, as used in curl (can specify multiple)
 	-o	output directory
-	-n	chunks to split file into for download via HTTP Range header
+	-n	chunks to split file into for each remote (default 10)
 	-v	verbose
 
 	example: ./fetchfork.py -r server1,user@server2 -H "Cookie: Authorization 123" https://website.com/file.zip
@@ -25,6 +25,7 @@ def main():
 	headers = {}
 	remotes = []
 	assume_diskfree_mb = 200
+	chunks = 10
 	outdir = None
 	verbose = False
 
@@ -48,6 +49,8 @@ def main():
 			remotes = a.replace(" ",'').split(',')
 		elif o in ("-o", "--output"):
 			outdir = a
+		elif o in ("-n"):
+			chunks = int(a)
 		else:
 			assert False, "unhandled option"
 
@@ -60,7 +63,7 @@ def main():
 		usage()
 		exit()
 
-	remotes_diskfree_mb = {}
+	remotes_diskfree = {}
 	curl_remotes = set()
 	wget_remotes = set()
 	# initiate checks
@@ -88,31 +91,68 @@ def main():
 		out = p.stdout + p.stderr
 		if "not found" in out.lower():
 			eprint(f"unable to determine disk space on {remote}, assuming {assume_diskfree}MB")
-			remotes_diskfree_mb[r] = assume_diskfree_mb * 1000000
+			remotes_diskfree[r] = assume_diskfree_mb * 1000000
 		else:
 			# get second line, 3rd element since it's usually the "Avail" table
 			df_avail_str = out.split("\n")[1].split()[3].strip()
 			if df_avail_str.endswith("G"): #gigabytes
-				remotes_diskfree_mb[r] = float(df_avail_str.split("G")[0]) * 1000000000
+				remotes_diskfree[r] = int(float(df_avail_str.split("G")[0]) * 1000000000)
 			if df_avail_str.endswith("M"): #megabytes
-				remotes_diskfree_mb[r] = float(df_avail_str.split("M")[0]) * 1000000
+				remotes_diskfree[r] = int((float(df_avail_str.split("M")[0]) - 5) * 1000000) # save 5M so system doesn crash
 			if df_avail_str.endswith("K"): #kilobytes
-				remotes_diskfree_mb[r] = float(df_avail_str.split("K")[0]) * 1000
+				remotes_diskfree[r] = int((float(df_avail_str.split("K")[0]) - 200) * 1000) # save 200K 
 			if verbose:
-				eprint(f"remote {r} has {remotes_diskfree_mb[r]} bytes of avail disk space")
+				eprint(f"remote {r} has {remotes_diskfree[r]} bytes of avail disk space")
 
 	# check how big the file is
 	eprint("using the first remote to check file size..")
 	file_size_bytes = -1
 	headers_out = ''
+	
+	
 	if remotes[0] in curl_remotes:
 		headers_out = subprocess.check_output(["ssh", r, f"curl -I '{url}'"],text=True)
 	elif remotes[0] in wget_remotes:
-		headers_out = subprocess.check_output(["ssh", r, f"wget -S --spider '{url}'"],text=True)
+		headers_out = subprocess.check_output(["ssh", r, f"wget -S --spider '{url}' 2>&1"],text=True)
 	for line in headers_out.split("\n"):
-		if line.lower().startswith("content-length"):
+		if line.strip().lower().startswith("content-length"):
 			file_size_bytes = float(line.split(": ")[1])
 	eprint(f"file is {file_size_bytes} bytes")
+
+	chunk_size = file_size_bytes // chunks
+	eprint("size of each main chunk:", chunk_size)
+	remotes_range_headers_map = {}
+	for r in remotes:
+		remotes_range_headers_map[r] = []
+	current_range_offset = int(0)
+	while current_range_offset < file_size_bytes - 1: # range is 0 indexed so 1000 bytes ends at offset 999
+		# distribution
+		for r in remotes: # split file into range headers for each remote to process single or multi threaded
+			if remotes_diskfree[r] > chunk_size:
+				if current_range_offset+chunk_size > file_size_bytes - 1:
+					remotes_range_headers_map[r].append(f"{int(current_range_offset)}-")
+					current_range_offset = file_size_bytes - 1 #done
+					break
+				else:
+					remotes_range_headers_map[r].append(f"{int(current_range_offset)}-{int(current_range_offset+chunk_size)}")
+					current_range_offset += chunk_size
+			else:
+				for i in chunk_size // remotes_diskfree[r]:
+					subchunk_size = remotes_diskfree[r]
+					if current_range_offset+subchunk_size > file_size_bytes - 1:
+						remotes_range_headers_map[r].append(f"{int(current_range_offset)}-")
+						current_range_offset =  file_size_bytes - 1 #done
+						break
+					else:
+						remotes_range_headers_map[r].append(f"{int(current_range_offset)}-{int(current_range_offset + subchunk_size)}")
+						current_range_offset += subchunk_size
+			eprint(current_range_offset)
+	if verbose:
+		eprint("chunks range headers map:")
+		for r in remotes:
+			eprint(r, remotes_range_headers_map[r])
+
+
 
 
 
